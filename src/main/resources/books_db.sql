274169c8a04e1a82203824924cb5430f53e29eb5
sanjay0206 <sanjayp0206@gmail.com>
2024-08-25 23:50:41 +0530

    new commit

--- src/main/resources/books_db.sql
+++ src/main/resources/books_db.sql
@@ -11,7 +11,7 @@ INSERT INTO books (book_id, name, summary, rating) VALUES
 (3, 'Grokking Algorithms', 'Learn algorithms the fun way', 5.0),
 (4, 'Cooking Fundamentals', 'Explores essential cooking principles and techniques to elevate your culinary skills.', 5.0),
 (5, 'Science of Cooking', 'Delves into the scientific aspects of cooking, providing insights to enhance your culinary expertise.', 4.5),
-(6, 'Italian Cuisine', 'Showcases classic Italian recipes and culinary traditions, offering a taste of Italy\'s rich culinary heritage.'', 4.0);
+(6, 'Italian Cuisine', 'Showcases classic Italian recipes and culinary traditions, offering a taste of Italy''s rich culinary heritage.', 4.0);
 
 
 CREATE OR REPLACE FUNCTION get_count_of_books(OUT total NUMERIC)
